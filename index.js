8c0fdb669916e1b3ea4468472ba2d8693c90072a
Ankur Singhal <aksinghal.aks@gmail.com>
2023-06-01 21:15:18 -0700

    full production build exercises 3.9 to 3.11

--- index.js
+++ index.js
@@ -3,14 +3,19 @@
 
 const express = require('express')
 const morgan = require('morgan')
-// const cors = require('cors') 
+const cors = require('cors') 
 
 const app = express()
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "Unknown endpoint."})
+}
 
 morgan.token('body', request => { return JSON.stringify(request.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
@@ -105,6 +110,9 @@ app.delete('/api/persons/:id', (request, response) => {
     return response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => { 
+    console.log(`Server running on port ${PORT}`)
+})
